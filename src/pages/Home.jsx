75a831a7c530e181073d5154034b7d5b2a9c9a85
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-23 19:11:09 -0300

    refact: refact home component creating gogh-frame component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,15 @@
-import React, { useContext } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { Link } from 'react-router-dom';
-import DarkVanGogh from '../assets/images/the_starry_night.jpg';
-import LightVanGogh from '../assets/images/green_wheat_field_with_cypress.jpg';
+import React from 'react';
+import { motion } from 'framer-motion';
 import './Home.scss';
 import Header from '../components/Header';
-import { ThemeContext } from '../context/ThemeProvider';
-
-const transition = { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
+import GoghFrame from '../components/GoghFrame';
 
 export default function Home() {
-  const { theme } = useContext(ThemeContext);
-
   return (
     <>
       <Header />
       <div className="home-wrapper">
-        <div className="img-frame">
-          <Link to="/hello">
-            <AnimatePresence mode="wait">
-              {theme === 'dark-mode' ? (
-                <motion.img
-                  key="dark"
-                  initial={{ opacity: 0, scale: 2 }}
-                  animate={{
-                    opacity: 1,
-                    transition: { duration: 1 },
-                  }}
-                  whileHover={{
-                    scale: 2.5,
-                  }}
-                  exit={{ opacity: 0, transition: { duration: 0.4 } }}
-                  transition={transition}
-                  src={DarkVanGogh}
-                />
-              ) : (
-                <motion.img
-                  key="light"
-                  initial={{ opacity: 0, scale: 2 }}
-                  animate={{
-                    opacity: 1,
-                    transition: { duration: 1 },
-                  }}
-                  whileHover={{
-                    scale: 2.5,
-                  }}
-                  exit={{ opacity: 0, transition: { duration: 0.4 } }}
-                  transition={transition}
-                  src={LightVanGogh}
-                />
-              )}
-            </AnimatePresence>
-          </Link>
-        </div>
+        <GoghFrame />
         <motion.p
           initial={{ opacity: 0 }}
           animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
