80c7f5ce31b469df844132afcaa76e9833455c28
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-23 18:09:35 -0300

    feat: exit animation on images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import DarkVanGogh from '../assets/images/the_starry_night.jpg';
 import LightVanGogh from '../assets/images/green_wheat_field_with_cypress.jpg';
@@ -18,18 +18,39 @@ export default function Home() {
       <div className="home-wrapper">
         <div className="img-frame">
           <Link to="/hello">
-            <motion.img
-              initial={{ opacity: 0, scale: 2 }}
-              animate={{
-                opacity: 1,
-                transition: { delay: 0.5, duration: 2 },
-              }}
-              whileHover={{
-                scale: 2.5,
-              }}
-              transition={transition}
-              src={theme === 'dark-mode' ? DarkVanGogh : LightVanGogh}
-            />
+            <AnimatePresence mode="wait">
+              {theme === 'dark-mode' ? (
+                <motion.img
+                  key="dark"
+                  initial={{ opacity: 0, scale: 2 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 1 },
+                  }}
+                  whileHover={{
+                    scale: 2.5,
+                  }}
+                  exit={{ opacity: 0, transition: { duration: 0.4 } }}
+                  transition={transition}
+                  src={DarkVanGogh}
+                />
+              ) : (
+                <motion.img
+                  key="light"
+                  initial={{ opacity: 0, scale: 2 }}
+                  animate={{
+                    opacity: 1,
+                    transition: { duration: 1 },
+                  }}
+                  whileHover={{
+                    scale: 2.5,
+                  }}
+                  exit={{ opacity: 0, transition: { duration: 0.4 } }}
+                  transition={transition}
+                  src={LightVanGogh}
+                />
+              )}
+            </AnimatePresence>
           </Link>
         </div>
         <motion.p
