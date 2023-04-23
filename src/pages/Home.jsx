996bb6aa5c14eb95b6b97efa8781057f4abb234c
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-23 10:33:22 -0300

    refact: refact some styles configurations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import DarkImg from '../assets/images/the_starry_night.png';
 import './Home.scss';
 
-const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+const transition = { duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
 
 export default function Home() {
   return (
@@ -12,7 +12,8 @@ export default function Home() {
       <div className="img-frame">
         <Link to="/hello">
           <motion.img
-            initial={{ scale: 1.1 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 0.5, duration: 2 } }}
             whileHover={{
               scale: 1.2,
             }}
@@ -22,6 +23,8 @@ export default function Home() {
         </Link>
       </div>
       <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
         className="username"
