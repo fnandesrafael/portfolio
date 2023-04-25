687a71d199a038745299ce2287bc0e3f53f3ba9c
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-25 10:35:53 -0300

    refact: theme now is default set by App rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Home from './pages/Home';
@@ -6,19 +6,29 @@ import Hello from './pages/Hello';
 import NotFound from './pages/NotFound';
 import './App.scss';
 import Header from './components/Header';
+import { ThemeContext } from './context/ThemeProvider';
 
 export default function App() {
+  const { theme } = useContext(ThemeContext);
+
   const location = useLocation();
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
 
   return (
     <div className="app-wrapper">
-      <Header />
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/hello" element={<Hello />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <AnimatePresence>
+        {location.pathname === '/' ? null : <Header />}
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Hello />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AnimatePresence>
       </AnimatePresence>
     </div>
   );
