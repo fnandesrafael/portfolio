f72c355aa70e6e7b9c6091f867d92076b3af7909
Rafael de Lima <rafaelima.fernandez@pm.me>
2023-04-25 10:36:23 -0300

    fix: remove useEffect from ThemeSwitcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../context/ThemeProvider';
 import MoonIcon from './MoonIcon';
 import SunIcon from './SunIcon';
@@ -7,11 +7,6 @@ import './ThemeSwitcher.scss';
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useContext(ThemeContext);
-
-  useEffect(() => {
-    document.body.className = theme;
-    localStorage.setItem('theme', JSON.stringify(theme));
-  }, [theme]);
 
   const switchTheme = () => {
     setTheme(theme === 'dark-mode' ? 'light-mode' : 'dark-mode');
