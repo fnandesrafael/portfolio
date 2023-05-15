bdefe07dcbb3bbfaccfc9fb81d74a2cbe44d1056
fnandesrafael <fnandesrafael@pm.me>
2023-05-15 11:55:13 -0300

    update: major updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         exit={{ opacity: 0, transition: { duration: 1 } }}
         className="home-name"
       >
-        Rafael de Lima
+        @fnandesrafael
       </motion.p>
     </div>
   );
