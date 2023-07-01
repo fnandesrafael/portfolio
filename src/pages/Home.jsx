0bef2365cfe17716f887ef40398103192ebac7bd
fnandesrafael <fnandesrafael@pm.me>
2023-07-01 15:11:32 -0300

    feat: mouse follower element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { motion } from 'framer-motion';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { animate, motion } from 'framer-motion';
 import GoghFrame from '../components/GoghFrame';
 import badgeLight from '../assets/images/badge_light.png';
 import badgeDark from '../assets/images/badge_dark.png';
@@ -9,14 +9,49 @@ import './Home.scss';
 
 export default function Home() {
   const { theme } = useContext(ThemeContext);
+  const [mouseMove, setMouseMove] = useState(false);
+  const mouseRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    setMouseMove(true);
+
+    const follower = mouseRef.current;
+
+    if (follower) {
+      const x = e.clientX;
+      const y = e.clientY;
+
+      follower.style.cssText = `top: ${y}px; left: ${x}px;`;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className="home-wrapper">
+      {mouseMove ? (
+        <motion.p
+          key="mouse-follower"
+          ref={mouseRef}
+          className="mouse-follower"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
+          exit={{ opacity: 0, transition: { duration: 1 } }}
+        >
+          enter the frame
+        </motion.p>
+      ) : null}
       <motion.img
         className="badge"
         src={theme === 'dark-mode' ? badgeDark : badgeLight}
+        key={`${theme}-badge`}
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { delay: 1, duration: 1 } }}
+        animate={{ opacity: 1, transition: { duration: 1 } }}
         exit={{ opacity: 0, transition: { duration: 1 } }}
       />
       <GoghFrame />
