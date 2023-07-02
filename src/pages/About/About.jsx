f9e58e9621b023a4cbe9f71eb762934e2fe59cd4
fnandesrafael <fnandesrafael@pm.me>
2023-07-01 23:32:41 -0300

    feat: first implementation of About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { ThemeContext } from '../../context/ThemeProvider';
 import DarkGogh from '../../assets/images/the_starry_night.jpg';
+import LightGogh from '../../assets/images/green_wheat_field_with_cypress.jpg';
 
 import './About.scss';
 
@@ -10,7 +11,45 @@ export default function About() {
 
   return (
     <div className="about-wrapper">
-      <img key={`${theme}-gogh-bg`} className="gogh-bg" src={DarkGogh} alt="" />
+      <motion.div
+        className="paint-frame"
+        animate={{
+          borderRadius: 0,
+          height: '110vh',
+          transition: { duration: 1.5, ease: [0.43, 0.13, 0.23, 0.96] },
+          width: '100%',
+        }}
+      >
+        {theme === 'dark-mode' ? (
+          <motion.img
+            className="gogh-paint"
+            src={DarkGogh}
+            initial={{ y: -10, scale: 2.5 }}
+            animate={{
+              y: 0,
+              filter: 'saturate(0)',
+              transition: {
+                duration: 1.5,
+                ease: [0.43, 0.13, 0.23, 0.96],
+              },
+            }}
+          />
+        ) : (
+          <motion.img
+            className="gogh-paint"
+            src={LightGogh}
+            initial={{ y: 0, scale: 2.5 }}
+            animate={{
+              y: -10,
+              filter: 'saturate(0)',
+              transition: {
+                duration: 1.5,
+                ease: [0.43, 0.13, 0.23, 0.96],
+              },
+            }}
+          />
+        )}
+      </motion.div>
     </div>
   );
 }
