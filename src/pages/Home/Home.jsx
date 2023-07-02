df445bbdf063f81a0b1f484672bb374afb18c6dc
fnandesrafael <fnandesrafael@pm.me>
2023-07-02 10:20:51 -0300

    update: follower element text

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -46,7 +46,7 @@ export default function Home() {
           animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
           exit={{ opacity: 0, transition: { duration: 1 } }}
         >
-          click on the frame and rest
+          click on the frame and stay on it
         </motion.p>
       ) : null}
       <motion.img
