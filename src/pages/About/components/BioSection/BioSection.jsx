7322199b42e3842e2315aa2aaf777c6c5456619c
fnandesrafael <fnandesrafael@pm.me>
2023-07-09 17:07:26 -0300

    feat: BioSection.jsx first implementation

--- src/pages/About/components/BioSection/BioSection.jsx
+++ src/pages/About/components/BioSection/BioSection.jsx
@@ -3,5 +3,19 @@ import React from 'react';
 import './BioSection.scss';
 
 export default function BioSection() {
-  return <div className="bio-wrapper">BioSection</div>;
+  return (
+    <div className="bio-wrapper">
+      <div className="bio-l-container"></div>
+      <div className="bio-r-container">
+        <div className="upper-r-containers">
+          <div className="upper-l"></div>
+          <div className="upper-r"></div>
+        </div>
+        <div className="bottom-r-containers">
+          <div className="bottom-l"></div>
+          <div className="bottom-r"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
