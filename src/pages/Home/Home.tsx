831603f511cab1be08f7aff8080bc5a08b0f810b
fnandesrafael <fnandesrafael@pm.me>
2023-08-18 13:18:58 -0300

    refact: MouseFollower component in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import badgeLight from '@assets/images/badge_light.png';
 import badgeDark from '@assets/images/badge_dark.png';
 import { ThemeContext } from 'styled-components';
+import { MouseFollower } from '@components/MouseFollower';
 import GoghFrame from './components/GoghFrame';
 
 import './Home.scss';
@@ -66,46 +67,23 @@ const secondNameVariant = {
 
 export default function Home() {
   const { theme } = useContext(ThemeContext);
-  const [mouseMove, setMouseMove] = useState(false);
-  const mouseRef = useRef(null);
-
-  const handleMouseMove = (e) => {
-    setMouseMove(true);
-
-    const follower = mouseRef.current;
-
-    if (follower) {
-      const x = e.clientX;
-      const y = e.clientY;
-
-      follower.style.cssText = `top: ${y}px; left: ${x}px;`;
-    }
-  };
 
   useEffect(() => {
     document.body.style.cssText = `overflow: hidden; touch-action: none;`;
-    document.addEventListener('mousemove', handleMouseMove);
 
     return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
       document.body.style.cssText = `overflow: auto; touch-action: auto`;
     };
   }, []);
 
   return (
     <div className="home-wrapper">
-      {mouseMove ? (
-        <motion.p
-          key="mouse-follower"
-          ref={mouseRef}
-          className="mouse-follower"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
-          exit={{ opacity: 0, transition: { duration: 1 } }}
-        >
+      <MouseFollower.Root>
+        <MouseFollower.Follower>
           click on the frame and stay on it
-        </motion.p>
-      ) : null}
+        </MouseFollower.Follower>
+      </MouseFollower.Root>
+
       <motion.img
         key={`${theme}-badge`}
         className="badge"
