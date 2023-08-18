4e01a0c5fbfd0f7d48738adc81d7423b4f3a74fe
fnandesrafael <fnandesrafael@pm.me>
2023-08-18 12:09:54 -0300

    fix: theme keys and values

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -3,7 +3,10 @@ export default {
   colors: {
     primary: '#131313',
     secundary: '#FEC684',
-    background: '#131313',
-    text: '#B3B0AA',
+    textPrimary: '#FEC684',
+    textSecundary: '#131313',
+    textTerciary: '#B3B0AA',
+    background: '#FEC684',
+    highlight: '#131313',
   },
 };
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,9 +1,12 @@
 export default {
   title: 'light',
   colors: {
-    primary: '#FEC684',
+    primary: '#B3B0AA',
     secundary: '#131313',
-    background: '#B3B0AA',
-    text: '#131313',
+    textPrimary: '#131313',
+    textSecundary: '#D9D9D9',
+    textTerciary: '#FEC684',
+    background: '#131313',
+    highlight: '#FEC684',
   },
 };
