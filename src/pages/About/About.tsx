ce8905c5d99a4123f6da6b11475fea85c79041a0
fnandesrafael <fnandesrafael@pm.me>
2023-08-21 16:35:09 -0300

    refact: adding new Signature to hero section on About page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,70 @@
 import React, { useContext, useEffect } from 'react';
-import { useScroll, useTransform } from 'framer-motion';
+import { Variants, useScroll, useTransform } from 'framer-motion';
 import { ThemeContext } from 'styled-components';
 import darkPaint from '@assets/images/the_starry_night.jpg';
 import lightPaint from '@assets/images/green_wheat_field_with_cypress.jpg';
 import { GoghFrame } from '@components/GoghFrame';
 import { ScrollIcon } from '@components/ScrollIcon';
+import { Signature } from '@components/Signature';
 
 import * as S from './styles';
+
+const letterVariant: Variants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: 100,
+    transition: {
+      duration: 0.8,
+      ease: [0.43, 0.13, 0.23, 0.96],
+    },
+  },
+};
+
+const firstNameVariant: Variants = {
+  animate: {
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+    },
+  },
+  exit: {
+    transition: {
+      delayChildren: 0,
+      staggerChildren: 0.04,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const lastNameVariant: Variants = {
+  animate: {
+    transition: {
+      delayChildren: 1,
+      staggerChildren: 0.04,
+      staggerDirection: 1,
+    },
+  },
+  exit: {
+    transition: {
+      delayChildren: 0,
+      staggerChildren: 0.04,
+      staggerDirection: 1,
+    },
+  },
+};
 
 export default function About() {
   const theme = useContext(ThemeContext);
@@ -90,6 +148,28 @@ export default function About() {
       <ScrollIcon.Root>
         <ScrollIcon.Icon />
       </ScrollIcon.Root>
+
+      <Signature.Root
+        props={{
+          styles: {
+            display: 'flex',
+            flexFlow: 'column',
+            alignItems: 'flex-start',
+            zIndex: 10,
+          },
+        }}
+      >
+        <Signature.Text
+          props={{ wordVariants: firstNameVariant, letterVariant }}
+        >
+          Rafael
+        </Signature.Text>
+        <Signature.Text
+          props={{ wordVariants: lastNameVariant, letterVariant }}
+        >
+          Fernandes
+        </Signature.Text>
+      </Signature.Root>
     </S.PageWrapper>
   );
 }
