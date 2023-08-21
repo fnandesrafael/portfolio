517b08c5e06bb0687643031ef6805658d5699ffc
fnandesrafael <fnandesrafael@pm.me>
2023-08-21 12:10:28 -0300

    feat: add decorator path to constants folder

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@context': path.resolve(__dirname, './src/context'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@hooks': path.resolve(__dirname, './src/hooks')
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@constants': path.resolve(__dirname, 'src/constants')
     },
   },
 })
