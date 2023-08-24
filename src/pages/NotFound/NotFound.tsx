8fab61224b09e7c7ad921f3624984aa9dbb7f0e5
fnandesrafael <fnandesrafael@pm.me>
2023-08-24 18:59:39 -0300

    feat: Navigation componente to  NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect } from 'react';
 
+import Navigation from '@components/Navigation';
 import * as S from './styles';
 
 export default function NotFound() {
   useEffect(() => {
     window.scrollTo(0, 0);
-    document.body.style.cssText = 'overflow: hidden';
   }, []);
 
   return (
     <S.PageWrapper>
-      <S.ErrorElement
-        className="not-found-code"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 1 } }}
-      >
-        404
-      </S.ErrorElement>
+      <S.ErrorWrapper>
+        <S.ErrorElement
+          className="not-found-code"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 1 } }}
+        >
+          404
+        </S.ErrorElement>
+      </S.ErrorWrapper>
+
+      <Navigation.Root>
+        <Navigation.NavLink props={{ page: '/home' }}>Home</Navigation.NavLink>
+        <Navigation.NavLink props={{ page: '/about' }}>
+          About
+        </Navigation.NavLink>
+        <Navigation.NavLink props={{ page: '/resume' }}>
+          Resume
+        </Navigation.NavLink>
+      </Navigation.Root>
     </S.PageWrapper>
   );
 }
