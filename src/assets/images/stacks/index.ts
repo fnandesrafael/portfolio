16d77ed405f9221a504fa523efbf5d8079d3bb94
fnandesrafael <fnandesrafael@pm.me>
2023-09-04 10:40:02 -0300

    feat: new framess

--- src/assets/images/stacks/index.ts
+++ src/assets/images/stacks/index.ts
@@ -1,11 +1,35 @@
+import axios from './axios.png';
 import css from './css.png';
+import docker from './docker.png';
+import eslint from './eslint.png';
 import framer from './framer.png';
 import html from './html.png';
 import javascript from './javascript.png';
 import jest from './jest.png';
 import react from './react.png';
+import router from './router.png';
+import rtl from './rtl.png';
 import scss from './scss.png';
 import styled from './styled.png';
+import stylelint from './stylelint.png';
 import typescript from './typescript.png';
+import vite from './vite.png';
 
-export { css, framer, html, javascript, jest, react, scss, styled, typescript };
+export {
+  axios,
+  css,
+  docker,
+  eslint,
+  framer,
+  html,
+  javascript,
+  jest,
+  react,
+  router,
+  rtl,
+  scss,
+  styled,
+  stylelint,
+  typescript,
+  vite,
+};
