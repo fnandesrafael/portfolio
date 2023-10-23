1fec35ef4e784e1928475f430990b6b565389bfd
fnandesrafael <fnandesrafael@pm.me>
2023-10-23 18:42:10 -0300

    feat: Projects page

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,11 +1,10 @@
 import React, { useLayoutEffect } from 'react';
 import { EASE_SWEET } from '@constants/animations';
 import { Navigator } from '@components/Navigator';
-import { ScrollIcon } from '@components/ScrollIcon';
 
 import * as S from './styles';
 
-export default function NotFound() {
+export default function Projects() {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
 
@@ -23,18 +22,90 @@ export default function NotFound() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1, ease: EASE_SWEET }}
       >
-        <S.ErrorWrapper>
-          <S.ErrorElement
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 1 } }}
-          >
-            Work In Progress
-          </S.ErrorElement>
-        </S.ErrorWrapper>
+        <S.PageHeading>Projects</S.PageHeading>
 
-        <ScrollIcon.Root>
-          <ScrollIcon.Icon />
-        </ScrollIcon.Root>
+        <S.Container>
+          <S.Projects>
+            <S.Header>
+              <div>
+                <S.HeaderParagraph>Project</S.HeaderParagraph>
+              </div>
+              <div>
+                <S.HeaderParagraph>Category</S.HeaderParagraph>
+              </div>
+              <div>
+                <S.HeaderParagraph>Stack</S.HeaderParagraph>
+              </div>
+              <div>
+                <S.HeaderParagraph>Year</S.HeaderParagraph>
+              </div>
+            </S.Header>
+            <a href="https://openshopio.vercel.app/" target="_blank">
+              <S.Project id="p1">
+                <div>
+                  <S.Paragraph>Openshop</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>E-commerce</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Frontend</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>2023</S.Paragraph>
+                </div>
+              </S.Project>
+            </a>
+            <a href="https://memoryboardio.vercel.app" target="_blank">
+              <S.Project id="p2">
+                <div>
+                  <S.Paragraph>Memory Board</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Tool</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Frontend</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>2023</S.Paragraph>
+                </div>
+              </S.Project>
+            </a>
+            <a href="https://weatherappio.vercel.app" target="blank">
+              <S.Project id="p3">
+                <div>
+                  <S.Paragraph>Weather App</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Tool</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Frontend</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>2023</S.Paragraph>
+                </div>
+              </S.Project>
+            </a>
+            <a href="https://www.npmjs.com/package/parrotenv" target="_blank">
+              <S.Project id="p4">
+                <div>
+                  <S.Paragraph>ParrotEnv</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Framework</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>Backend</S.Paragraph>
+                </div>
+                <div>
+                  <S.Paragraph>2023</S.Paragraph>
+                </div>
+              </S.Project>
+            </a>
+          </S.Projects>
+        </S.Container>
       </S.PageWrapper>
 
       <Navigator.Root>
--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,21 +1,109 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-
-export const ErrorElement = styled(motion.h1)`
-  font-size: 2rem;
-  font-weight: 400;
-  transition: 0.8s color;
-`;
-
-export const ErrorWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-flow: column;
-  height: 100vh;
-  justify-content: center;
-`;
+import projects from '@assets/images/projects.png';
 
 export const PageWrapper = styled(motion.main)`
+  @media screen and (height > 768px) {
+    margin-top: 18rem;
+    margin-bottom: 6rem;
+    scale: 1.4;
+  }
+
+  color: ${({ theme }) => theme.colors.textPrimary};
   display: flex;
   flex-flow: column;
+  justify-content: center;
+  margin-top: 6rem;
+  transition: 0.6s color;
+`;
+
+export const Preview = styled.div`
+  height: 250px;
+  overflow: hidden;
+  pointer-events: none;
+  position: absolute;
+  transform: scale(0);
+  transform-origin: center;
+  width: 250px;
+  z-index: 10;
+`;
+
+export const PreviewImage = styled.img`
+  width: 100%;
+  height: 100%;
+  background: ${`url(${projects})`};
+  background-size: cover;
+  background-position: 0 0;
+  pointer-events: none;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 1.25rem;
+  line-height: 100%;
+  text-transform: uppercase;
+`;
+
+export const HeaderParagraph = styled(Paragraph)`
+  opacity: 0.5;
+`;
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 97%;
+`;
+
+export const Projects = styled.div`
+  display: flex;
+  width: 70%;
+  flex-flow: column;
+`;
+
+export const Project = styled.div`
+  align-items: center;
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.background}`};
+  cursor: pointer;
+  display: flex;
+  padding: 2.5rem 1rem;
+  transition: 0.6s border-bottom, 0.6s opacity;
+  width: 100%;
+
+  &:hover {
+    opacity: 0.5;
+  }
+
+  & > div {
+    flex: 3;
+  }
+
+  & div:last-child {
+    flex: 1;
+  }
+`;
+
+export const Header = styled(Project)`
+  cursor: default;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const PageHeading = styled.h1`
+  @media screen and (height > 768px) {
+    top: -16rem;
+    font-size: 42rem;
+  }
+
+  display: flex;
+  font-size: 24.25rem;
+  font-weight: 400;
+  left: -4rem;
+  opacity: 0.025;
+  pointer-events: none;
+  position: absolute;
+  top: -10rem;
+  z-index: 0;
 `;
